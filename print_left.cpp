596238d80dc8966b27f5b946d29a30bd325c3b85
Abhineet Mishra <abhineetmishra64@gmail.com>
2021-06-27 23:19:51 +0530

    Print the left element

---
+++ print_left.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    int leftElement(int a[], int n) {
+        // Your code goes here 
+        int med,i,j;
+        sort(a,a+n);
+        int size=n/2;
+        if(n%2==0){
+            return a[size-1];
+        }
+        else return a[size];
+    }
+};  
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.leftElement(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
