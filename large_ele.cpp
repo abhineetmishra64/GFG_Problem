c5a82bbce34543ba8f1409b8fe536d97161c2ce4
Abhineet Mishra <abhineetmishra64@gmail.com>
2021-07-01 23:22:40 +0530

    Add files via upload

---
+++ large_ele.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+using namespace std;
+
+int largest(int arr[], int n) 
+{
+    int max = arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>max) max = arr[i];
+    }
+    return max;
+}
+
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++) scanf("%d",&arr[i]);
+    printf("%d",largest(arr,n));
+    printf("\n");
+    return 0;
+}
